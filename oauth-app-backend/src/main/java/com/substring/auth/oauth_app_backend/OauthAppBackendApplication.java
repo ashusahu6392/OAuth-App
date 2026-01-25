d7df95b98ebf0338591aaa0762c7d32e4fa5a371
Ashu Sahu <145501021+ashusahu6392@users.noreply.github.com>
2026-01-25 18:23:47 +0530

    Add main Spring Boot application class
    Introduces OauthAppBackendApplication as the entry point for the Spring Boot application.

---
+++ oauth-app-backend/src/main/java/com/substring/auth/oauth_app_backend/OauthAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.substring.auth.oauth_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthAppBackendApplication.class, args);
+	}
+
+}
