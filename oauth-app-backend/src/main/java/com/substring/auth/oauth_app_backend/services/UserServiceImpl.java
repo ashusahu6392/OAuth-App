be9a5fa6a85702fca628ab3976dfde5fe9c602b9
Ashu Sahu <145501021+ashusahu6392@users.noreply.github.com>
2026-01-26 23:45:52 +0530

    Add UserServiceImpl with method stubs
    Introduces the UserServiceImpl class implementing the UserService interface with placeholder methods for user CRUD operations. Actual logic is yet to be implemented.

---
+++ oauth-app-backend/src/main/java/com/substring/auth/oauth_app_backend/services/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.substring.auth.oauth_app_backend.services;
+
+import org.springframework.stereotype.Service;
+
+import com.substring.auth.oauth_app_backend.dtos.UserDto;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserDto createUser(UserDto userDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserDto getUserByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserDto updateUser(UserDto userDto, String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteUser(String userId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public UserDto getUserById(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<UserDto> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
