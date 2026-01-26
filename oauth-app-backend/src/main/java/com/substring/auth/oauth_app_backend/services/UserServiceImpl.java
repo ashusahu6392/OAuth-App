319da4ec54127deb4b778121e3d2e0e6dd1af253
Ashu Sahu <145501021+ashusahu6392@users.noreply.github.com>
2026-01-27 00:05:25 +0530

    Inject UserRepository into UserServiceImpl
    Added UserRepository as a final field in UserServiceImpl and enabled constructor injection using Lombok's @RequiredArgsConstructor. This prepares the service for repository-based operations.

--- oauth-app-backend/src/main/java/com/substring/auth/oauth_app_backend/services/UserServiceImpl.java
+++ oauth-app-backend/src/main/java/com/substring/auth/oauth_app_backend/services/UserServiceImpl.java
@@ -1,13 +1,19 @@
 package com.substring.auth.oauth_app_backend.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.substring.auth.oauth_app_backend.dtos.UserDto;
+import com.substring.auth.oauth_app_backend.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
 
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-
+	
+	private final UserRepository userRepository;
 	@Override
 	public UserDto createUser(UserDto userDto) {
 		// TODO Auto-generated method stub
