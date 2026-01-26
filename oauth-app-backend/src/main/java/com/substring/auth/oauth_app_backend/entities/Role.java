01414d3e424e5ec9da083a25afe6b97ef3de672a
Ashu Sahu <145501021+ashusahu6392@users.noreply.github.com>
2026-01-26 14:35:32 +0530

    Add Role entity for user roles
    Introduces the Role entity with fields for id and name, including JPA annotations and Lombok for boilerplate code. This sets up the foundation for managing user roles in the application.

---
+++ oauth-app-backend/src/main/java/com/substring/auth/oauth_app_backend/entities/Role.java
@@ -0,0 +1,30 @@
+package com.substring.auth.oauth_app_backend.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	@Id
+	private UUID id = UUID.randomUUID();
+	@Column(unique = true, nullable = false, length = 50)
+	private String name;
+}
