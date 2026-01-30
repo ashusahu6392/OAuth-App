3cf8a54b199e86709794c1e129fbc65f6e1cad57
Ashu Sahu <145501021+ashusahu6392@users.noreply.github.com>
2026-01-31 02:09:32 +0530

    Update UserServiceImpl.java

--- oauth-app-backend/src/main/java/com/substring/auth/oauth_app_backend/services/UserServiceImpl.java
+++ oauth-app-backend/src/main/java/com/substring/auth/oauth_app_backend/services/UserServiceImpl.java
@@ -1,10 +1,15 @@
 package com.substring.auth.oauth_app_backend.services;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import com.substring.auth.oauth_app_backend.dtos.UserDto;
+import com.substring.auth.oauth_app_backend.entities.Provider;
+import com.substring.auth.oauth_app_backend.entities.User;
+import com.substring.auth.oauth_app_backend.exception.ResourceNotFoundException;
 import com.substring.auth.oauth_app_backend.repositories.UserRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 
@@ -13,16 +18,30 @@ import lombok.RequiredArgsConstructor;
 public class UserServiceImpl implements UserService {
 	
 	private final UserRepository userRepository;
+	private final ModelMapper modelMapper;
 	@Override
+	@Transactional
 	public UserDto createUser(UserDto userDto) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		if(userDto.getEmail() == null || userDto.getEmail().isBlank()) {
+			throw new IllegalArgumentException("Email is required");
+		}
+		if(userRepository.existsByEmail(userDto.getEmail())) {
+			throw new IllegalArgumentException("Email already exists");
+		}
+		User user = modelMapper.map(userDto, User.class) ;
+		user.setProvider(userDto.getProvider() != null ? userDto.getProvider() : Provider. LOCAL) ;
+		User savedUser = userRepository.save(user) ;
+		return modelMapper.map(savedUser, UserDto.class) ;
 	}
 
 	@Override
 	public UserDto getUserByEmail(String email) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		User user = userRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("User not Found with given ID") );
+		return modelMapper.map(user, UserDto.class);
 	}
 
 	@Override
