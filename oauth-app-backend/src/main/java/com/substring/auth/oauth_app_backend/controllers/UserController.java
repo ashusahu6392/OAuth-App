36f959a53c88620cef49eea8c4f5a66388a79c9d
Ashu Sahu <145501021+ashusahu6392@users.noreply.github.com>
2026-02-01 02:00:27 +0530

    Add update and get user by ID endpoints
    Introduced PUT and GET endpoints in UserController to allow updating user details and retrieving a user by ID. These endpoints delegate to the userService for the respective operations.

--- oauth-app-backend/src/main/java/com/substring/auth/oauth_app_backend/controllers/UserController.java
+++ oauth-app-backend/src/main/java/com/substring/auth/oauth_app_backend/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +48,14 @@ public class UserController {
 		userService.deleteUser(userId);
 	}
 	
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") String userId){
+		return ResponseEntity.ok(userService.updateUser(userDto, userId));
+	}
 	
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> getUserById(@PathVariable("userId") String userId){
+		return ResponseEntity.ok(userService.getUserById(userId));
+	}
 	
 }
