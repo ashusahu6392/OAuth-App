3628e56300096ba450e5d2cbc4a12f09f51e67b1
Ashu Sahu <145501021+ashusahu6392@users.noreply.github.com>
2026-02-03 22:20:15 +0530

    Enable Spring Security and update AuthController
    Uncomment and add spring-boot-starter-security dependency. Refactor AuthController to accept @RequestBody UserDto, inject AuthService (via Lombok @AllArgsConstructor), and return HTTP 201 with the created user from authService.registerUser. Remove the old UserServiceImpl implementation (deleted) as part of the service refactor.

--- oauth-app-backend/src/main/java/com/substring/auth/oauth_app_backend/controllers/AuthController.java
+++ oauth-app-backend/src/main/java/com/substring/auth/oauth_app_backend/controllers/AuthController.java
@@ -1,19 +1,28 @@
 package com.substring.auth.oauth_app_backend.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.substring.auth.oauth_app_backend.dtos.UserDto;
+import com.substring.auth.oauth_app_backend.services.AuthService;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@AllArgsConstructor
 public class AuthController {
 	
+	private final AuthService authService;
+	
 	@PostMapping("/register")
-	public ResponseEntity<UserDto> registerUser(UserDto userDto){
-		return ResponseEntity.ok(userDto);
+	public ResponseEntity<UserDto> registerUser(@RequestBody UserDto userDto){
+		return ResponseEntity.status(HttpStatus.CREATED).body(authService.registerUser(userDto));
 	}
 
 }
