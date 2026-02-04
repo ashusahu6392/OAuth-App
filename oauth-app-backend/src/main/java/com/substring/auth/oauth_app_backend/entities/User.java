19c6fd4c206aa50f851ac937053e442247087888
Ashu Sahu <145501021+ashusahu6392@users.noreply.github.com>
2026-02-05 02:01:00 +0530

    Make User implement Spring Security UserDetails
    Adapt User entity to integrate with Spring Security by implementing UserDetails. Added imports and methods: getAuthorities (maps roles to SimpleGrantedAuthority), getUsername (returns email), isAccountNonExpired, isAccountNonLocked, isCredentialsNonExpired (all true), and isEnabled (uses enable field). This allows User to be used directly as the authenticated principal.

--- oauth-app-backend/src/main/java/com/substring/auth/oauth_app_backend/entities/User.java
+++ oauth-app-backend/src/main/java/com/substring/auth/oauth_app_backend/entities/User.java
@@ -2,9 +2,15 @@ package com.substring.auth.oauth_app_backend.entities;
 
 
 import java.time.Instant;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -36,7 +42,7 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
 	@Column(name = "user_id")
@@ -70,7 +76,36 @@ public class User {
 	protected void onUpdate() {
 		updatedAt = Instant.now();
 	}
-	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<SimpleGrantedAuthority> authorities = roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).toList();
+	    return authorities;
+	}
+
+	@Override
+	public String getUsername() {
+	    return this.email;   // Spring Security uses this for login
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+	    return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+	    return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+	    return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+	    return this.enable;
+	}
 
 }
 
